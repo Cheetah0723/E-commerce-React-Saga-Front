a8b842fef808d1def160b1b1cea6f63a4603ad2e
Cheetah0723 <kimuraa.hideyo@gmail.com>
2020-09-19 10:01:12 +1000

    deployed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import { Router } from 'react-router-dom';
+import {  BrowserRouter as Router   } from 'react-router-dom';
 import Routes from './Routes';
 import history from './history';
 import './fonts/Montserrat/Montserrat-Regular.ttf';

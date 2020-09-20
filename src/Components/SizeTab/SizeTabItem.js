9626d130bd5d0c9e1ea8244deb3e90e293aaf6a5
Cheetah0723 <kimuraa.hideyo@gmail.com>
2020-09-20 19:08:16 +1000

    Fixed Size Tab

--- src/Components/SizeTab/SizeTabItem.js
+++ src/Components/SizeTab/SizeTabItem.js
@@ -1,13 +1,10 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import ToggleButton from '@material-ui/lab/ToggleButton';
 
 export default function SizeTabItem({ value, disabled, clicked, handleClick }) {
-    const [isClicked, setClicked] = useState(clicked)
-
-    useEffect(() => { setClicked(clicked) }, [clicked])
 
     return (
-        <ToggleButton className={isClicked ? "button-group-item-clicked" : !disabled ? "button-group-item" : "disabledTab"}
+        <ToggleButton className={clicked ? "button-group-item-clicked" : !disabled ? "button-group-item" : "disabledTab"}
             value={value} key={value} onClick={handleClick}
             disabled={disabled}>{value}</ToggleButton>);
 }

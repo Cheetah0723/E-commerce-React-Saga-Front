5d8e1622d6d58635c66642f9a48a5d9487d6910e
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-02-07 21:08:34 +1100

    fixed nav bar mobile view

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Navbar, Nav, Form } from 'react-bootstrap';
+import Button from '@material-ui/core/Button';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import CurrencyHOC from "../Currency/CurrencyHOC"
+import MenuIcon from '@material-ui/icons/Menu';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/core/styles';
+import history from "../../history"
 
 const NavBar = ({ openDrawer }) => (
     <Navbar className="nav-bar" bg="light" variant="light">
-        <Navbar.Brand href="/">Logo</Navbar.Brand>
+        <Navbar.Brand className="logo" href="/">Logo</Navbar.Brand>
         <Nav className="nav-bar-links">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/all">Shop</Nav.Link>
@@ -13,15 +23,96 @@ const NavBar = ({ openDrawer }) => (
         </Nav>
         <Form inline className="ml-auto">
             <CurrencyHOC />
-            <div className="cart-icon  mr-sm-3" onClick={openDrawer} >
-                <ShoppingCartIcon />
-                <p>My Cart</p>
-            </div>
+            <Cart handleClick={openDrawer} />
         </Form>
         <div className="mobile-menu">
-            
+            <MobileMenu handleClick={openDrawer} />
         </div>
     </Navbar>
 );
+
+const Cart = ({ handleClick }) => (<div className="cart-icon  mr-sm-3" onClick={handleClick} >
+    <ShoppingCartIcon />
+    <p>My Cart</p>
+</div>)
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    paper: {
+        marginRight: theme.spacing(2),
+    },
+}));
+
+const MobileMenu = ({ handleClick }) => {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+
+    return (
+        <div className={classes.root}>
+            <div>
+                <Button
+                    ref={anchorRef}
+                    aria-controls={open ? 'menu-list-grow' : undefined}
+                    aria-haspopup="true"
+                    onClick={handleToggle}
+                >
+                    <MenuIcon />
+                </Button>
+                <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                    {({ TransitionProps, placement }) => (
+                        <Grow
+                            {...TransitionProps}
+                            style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                        >
+                            <Paper>
+                                <ClickAwayListener onClickAway={handleClose}>
+                                    <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                        <MenuItem onClick={() => history.push("/")}>Home</MenuItem>
+                                        <MenuItem onClick={() => history.push("/all")}>Shop</MenuItem>
+                                        <MenuItem onClick={() => history.push("/sales")}>Sales</MenuItem>
+                                        <MenuItem><CurrencyHOC /></MenuItem>
+                                        <MenuItem><Cart handleClick={handleClick} /></MenuItem>
+                                    </MenuList>
+                                </ClickAwayListener>
+                            </Paper>
+                        </Grow>
+                    )}
+                </Popper>
+            </div>
+        </div>
+    );
+}
 
 export default NavBar;

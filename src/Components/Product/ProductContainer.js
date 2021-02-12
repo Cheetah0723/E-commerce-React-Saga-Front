627c1be269a286a0b8349a9876864271c9fcad88
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-02-12 14:23:13 +1100

    fixed scss for deployment

--- src/Components/Product/ProductContainer.js
+++ src/Components/Product/ProductContainer.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import history from '../../history';
 import { useSelector } from "react-redux"
 import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
 import { selectCurrency } from '../Selectors';
 import { Col } from 'reactstrap';
 
@@ -19,23 +18,21 @@ export default function ProductContainer({ item }) {
         <Card className="ProductContainer" key={item.id} onClick={() => {
             history.push("/product/" + item.id, item.id);
         }}>
-            <img className="card-image" src= {item.img} alt={item.name} />
-                <Col>
-                    {item.salesRate < 1 && <Fragment>
-                        <p className="sales price-label">SALE</p>
-                        <p className="sales price"> {price * item.salesRate}{currency}</p>
-                    </Fragment>}
-                    {item.salesRate === 1 && <Fragment>
-                        <p className="price-label">New</p>
-                        <p className="price">${price}{currency}</p>
-                    </Fragment>
-                    }
+            <img className="card-image" src={item.img} alt={item.name} />
+            <Col>
+                {item.salesRate < 1 && <Fragment>
+                    <p className="sales price-label">SALE</p>
+                    <p className="sales price"> {price * item.salesRate}{currency}</p>
+                </Fragment>}
+                {item.salesRate === 1 && <Fragment>
+                    <p className="price-label">New</p>
+                    <p className="price">${price}{currency}</p>
+                </Fragment>
+                }
 
-                    <Typography className="item-name" variant="subtitle1" gutterBottom>
-                        {item.name}
-                    </Typography>
-                    <p className="item-color" >{item.color}</p>
-                </Col>
+                <p className="item-name" variant="subtitle1" gutterBottom>  {item.name}</p>
+                <p className="item-color" >{item.color}</p>
+            </Col>
         </Card>
     );
 }
